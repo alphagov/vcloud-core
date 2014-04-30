1475e2383e18bf29df10434f537283e5de9fed3c
Matt Bostock <matt@mattbostock.com>
2014-04-30 11:52:17 +0100

    Move Vcloud::Query to Vcloud::Core::Query
    Move `Vcloud::Query` to the `Vcloud::Core::Query` namespace so as to
    avoid conflicts with other Gems such as
    ['vcloud'](http://rubygems.org/gems/vcloud).

--- lib/vcloud/core/query.rb
+++ lib/vcloud/core/query.rb
@@ -1,98 +1,100 @@
 require 'csv'
 
 module Vcloud
-  class Query
+  module Core
+    class Query
 
-    def initialize(type=nil, options={}, query_runner = Vcloud::QueryRunner.new)
-      @type = type
-      @options = options
-      @options[:output_format] ||= 'tsv'
-      @query_runner = query_runner
-    end
-
-    def run()
-      if @type.nil?
-        output_available_query_types
-      else
-        output_query_results
+      def initialize(type=nil, options={}, query_runner = Vcloud::QueryRunner.new)
+        @type = type
+        @options = options
+        @options[:output_format] ||= 'tsv'
+        @query_runner = query_runner
       end
-    end
 
-    # <b>DEPRECATED:</b> Please use <tt>Vcloud::QueryRunner.run</tt> instead.
-    def get_all_results
-      warn "[DEPRECATION] `Vcloud::Query::get_all_results` is deprecated.  Please use `Vcloud::QueryRunner.run` instead."
-      @query_runner.run(@type, @options)
-    end
-
-  private
-    def output_query_results
-      results = @query_runner.run(@type, @options)
-      output_header(results)
-      output_results(results)
-    end
-
-    def output_available_query_types
-      available_query_types = @query_runner.available_query_types
-      available_queries = collate_formats_for_types(available_query_types)
-      print_query_types(available_queries)
-    end
-
-    def collate_formats_for_types(available_queries)
-      queries = Hash.new { |h, k| h[k]=[] }
-      available_queries.each do |type, format|
-        queries[type] << format
-      end
-      queries
-    end
-
-    def print_query_types(queries)
-      type_width = longest_query_type(queries)
-
-      queries.keys.sort.each do |type|
-        puts "%-#{type_width}s %s" % [type, queries[type].sort.join(',')]
-      end
-    end
-
-    def longest_query_type(queries)
-      return 0 if queries.keys.empty?
-      queries.keys.max_by{|key| key.length}.length
-    end
-
-    def output_header(results)
-      return if results.size == 0
-      case @options[:output_format]
-      when 'csv'
-        csv_string = CSV.generate do |csv|
-          csv << results.first.keys
+      def run()
+        if @type.nil?
+          output_available_query_types
+        else
+          output_query_results
         end
-        puts csv_string
-      when 'tsv'
-        puts results.first.keys.join("\t")
       end
-    end
 
-    def output_results(results)
-      return if results.size == 0
+      # <b>DEPRECATED:</b> Please use <tt>Vcloud::QueryRunner.run</tt> instead.
+      def get_all_results
+        warn "[DEPRECATION] `Vcloud::Core::Query::get_all_results` is deprecated.  Please use `Vcloud::QueryRunner.run` instead."
+        @query_runner.run(@type, @options)
+      end
 
-      case @options[:output_format]
-      when 'yaml'
-        puts YAML.dump(results)
-      when 'csv'
-        csv_string = CSV.generate do |csv|
-          results.each do |record|
-            csv << record.values
+      private
+      def output_query_results
+        results = @query_runner.run(@type, @options)
+        output_header(results)
+        output_results(results)
+      end
+
+      def output_available_query_types
+        available_query_types = @query_runner.available_query_types
+        available_queries = collate_formats_for_types(available_query_types)
+        print_query_types(available_queries)
+      end
+
+      def collate_formats_for_types(available_queries)
+        queries = Hash.new { |h, k| h[k]=[] }
+        available_queries.each do |type, format|
+          queries[type] << format
+        end
+        queries
+      end
+
+      def print_query_types(queries)
+        type_width = longest_query_type(queries)
+
+        queries.keys.sort.each do |type|
+          puts "%-#{type_width}s %s" % [type, queries[type].sort.join(',')]
+        end
+      end
+
+      def longest_query_type(queries)
+        return 0 if queries.keys.empty?
+        queries.keys.max_by{|key| key.length}.length
+      end
+
+      def output_header(results)
+        return if results.size == 0
+        case @options[:output_format]
+        when 'csv'
+          csv_string = CSV.generate do |csv|
+            csv << results.first.keys
           end
+          puts csv_string
+        when 'tsv'
+          puts results.first.keys.join("\t")
         end
-        puts csv_string
-      when 'tsv'
-        puts results.first.keys.join("\t") if @options[:page] == 1
-        results.each do |record|
-          puts record.values.join("\t")
-        end
-      else
-        raise "Unsupported output format #{@options[:output_format]}"
       end
-    end
 
+      def output_results(results)
+        return if results.size == 0
+
+        case @options[:output_format]
+        when 'yaml'
+          puts YAML.dump(results)
+        when 'csv'
+          csv_string = CSV.generate do |csv|
+            results.each do |record|
+              csv << record.values
+            end
+          end
+          puts csv_string
+        when 'tsv'
+          puts results.first.keys.join("\t") if @options[:page] == 1
+          results.each do |record|
+            puts record.values.join("\t")
+          end
+        else
+          raise "Unsupported output format #{@options[:output_format]}"
+        end
+      end
+
+    end
   end
 end
